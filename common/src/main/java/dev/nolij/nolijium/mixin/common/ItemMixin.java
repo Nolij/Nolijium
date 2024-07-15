cdbfa8a5a82277497041993200b184a780f66c84
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-07-14 21:56:14 -0400

    Improve compatibility of potion foil change

---
+++ common/src/main/java/dev/nolij/nolijium/mixin/common/ItemMixin.java
@@ -0,0 +1,16 @@
+package dev.nolij.nolijium.mixin.common;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import dev.nolij.nolijium.impl.Nolijium;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.PotionItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(Item.class)
+public class ItemMixin {
+	@ModifyReturnValue(method = "isFoil", at = @At("RETURN"))
+	private boolean makePotionsEnchantable(boolean original) {
+		return original || (Nolijium.config.revertPotions && (Object)this instanceof PotionItem);
+	}
+}
