89c2ceca6c5edba460877878ebf8d4384c21f2fd
Nolij <xdmatthewbx@mixhosting.xyz>
2024-08-24 13:49:51 -0400

    fix 20.1 crash caused by sky render toggle

---
+++ lexforge20/src/main/java/dev/nolij/nolijium/mixin/lexforge20/LevelRendererMixin.java
@@ -0,0 +1,26 @@
+package dev.nolij.nolijium.mixin.lexforge20;
+
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import com.mojang.blaze3d.vertex.PoseStack;
+import dev.nolij.nolijium.impl.Nolijium;
+import net.minecraft.client.Camera;
+import net.minecraft.client.renderer.LevelRenderer;
+import org.joml.Matrix4f;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(LevelRenderer.class)
+public class LevelRendererMixin {
+	
+	@WrapWithCondition(
+		method = "renderLevel",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/client/renderer/LevelRenderer;renderSky(Lcom/mojang/blaze3d/vertex/PoseStack;Lorg/joml/Matrix4f;FLnet/minecraft/client/Camera;ZLjava/lang/Runnable;)V"
+		)
+	)
+	public boolean nolijium$renderLevel$renderSky(LevelRenderer instance, PoseStack f8, Matrix4f f9, float j, Camera f3, boolean f4, Runnable f5) {
+		return !Nolijium.config.disableSkyRender;
+	}
+	
+}
--- neoforge/src/main/java/dev/nolij/nolijium/mixin/neoforge/LevelRendererMixin.java
+++ neoforge/src/main/java/dev/nolij/nolijium/mixin/neoforge/LevelRendererMixin.java
@@ -1,7 +1,11 @@
 package dev.nolij.nolijium.mixin.neoforge;
 
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import dev.nolij.nolijium.impl.Nolijium;
 import dev.nolij.nolijium.neoforge.NolijiumLightOverlayRenderer;
+import net.minecraft.client.Camera;
 import net.minecraft.client.renderer.LevelRenderer;
+import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,6 +19,17 @@ public class LevelRendererMixin {
 		NolijiumLightOverlayRenderer.invalidateSection(x, y, z);
 		// Invalidate section below, in case it now needs to show light levels on its topmost blocks
 		NolijiumLightOverlayRenderer.invalidateSection(x, y - 1, z);
+	}
+	
+	@WrapWithCondition(
+		method = "renderLevel",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/client/renderer/LevelRenderer;renderSky(Lorg/joml/Matrix4f;Lorg/joml/Matrix4f;FLnet/minecraft/client/Camera;ZLjava/lang/Runnable;)V"
+		)
+	)
+	public boolean nolijium$renderLevel$renderSky(LevelRenderer instance, Matrix4f matrix4f1, Matrix4f matrix4f2, float v, Camera camera, boolean b, Runnable runnable) {
+		return !Nolijium.config.disableSkyRender;
 	}
 	
 }
