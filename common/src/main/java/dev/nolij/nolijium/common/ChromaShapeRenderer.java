5624adcf72397ee35c9d3ec98bbe91f9d09de67e
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-09-09 21:16:04 -0400

    Sort chroma shape overlay correctly against translucent blocks in Fabulous! mode

--- common/src/main/java/dev/nolij/nolijium/common/ChromaShapeRenderer.java
+++ common/src/main/java/dev/nolij/nolijium/common/ChromaShapeRenderer.java
@@ -1,11 +1,14 @@
 package dev.nolij.nolijium.common;
 
+import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.blaze3d.vertex.VertexFormat;
 import dev.nolij.nolijium.impl.Nolijium;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.util.FastColor;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -16,8 +19,10 @@ public class ChromaShapeRenderer {
 	
 	private static final INolijiumSubImplementation NOLIJIUM_IMPL = Objects.requireNonNull(NolijiumCommon.getImplementation());
 	
+	private static final RenderType CHROMA_OVERLAY = RenderType.create("nolijium_chroma_overlay", DefaultVertexFormat.POSITION_COLOR, VertexFormat.Mode.TRIANGLE_STRIP, 1536, false, true, RenderType.CompositeState.builder().setShaderState(RenderStateShard.POSITION_COLOR_SHADER).setLayeringState(RenderStateShard.VIEW_OFFSET_Z_LAYERING).setOutputState(RenderStateShard.ITEM_ENTITY_TARGET).setTransparencyState(RenderStateShard.TRANSLUCENT_TRANSPARENCY).setWriteMaskState(RenderStateShard.COLOR_DEPTH_WRITE).setCullState(RenderStateShard.CULL).createCompositeState(false));
+	
 	private static void renderInnerOverlay(PoseStack poseStack, MultiBufferSource bufferSource, VoxelShape shape, float red, float green, float blue, float alpha) {
-		VertexConsumer boxConsumer = bufferSource.getBuffer(RenderType.debugFilledBox());
+		VertexConsumer boxConsumer = bufferSource.getBuffer(CHROMA_OVERLAY);
 		shape.forAllBoxes((x1, y1, z1, x2, y2, z2) -> 
 			LevelRenderer.addChainedFilledBoxVertices(poseStack, boxConsumer, x1 - 0.5, y1 - 0.5, z1 - 0.5, x2 - 0.5, y2 - 0.5, z2 - 0.5, red, green, blue, alpha));
 	}
